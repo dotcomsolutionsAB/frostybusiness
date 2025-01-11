56826e8b1f16fca16cae86ddef8cd99771e2761b
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2025-01-11 16:29:09 +0530

    s21

--- inc_files/top_header.php
+++ inc_files/top_header.php
@@ -2,7 +2,12 @@
     <div class="main-header__top-wrapper">
         <div class="main-header__top-inner">
             <div class="main-header__top-left">
-                <p class="main-header__top-text"><i class="icon-location"></i>Boral Main Road, Narendrapur, Kolkata-700 103</p>
+                <p class="main-header__top-text">
+                        <div class="icon">
+                            <i class="icon-location"></i>
+                        </div>
+                    <!-- <i class="icon-location"></i> -->
+                    Boral Main Road, Narendrapur, Kolkata-700 103</p>
                 <!-- <div class="main-header__social">
                     <a href="#"><i class="fab fa-twitter"></i></a>
                     <a href="#"><i class="fab fa-facebook"></i></a>
