9ded667268885e6c3b9e9a3501124f9423d65187
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2025-01-11 19:54:05 +0530

    s38

---
+++ inc_files/fav_code.php
@@ -0,0 +1,7 @@
+<title> Frosty Business </title>
+    <!-- favicons Icons -->
+<link rel="apple-touch-icon" sizes="180x180" href="images/favicons/apple-touch-icon.png" />
+<link rel="icon" type="image/png" sizes="32x32" href="images/favicon.ico"/>
+<link rel="icon" type="image/png" sizes="16x16" href="images/favicon.ico"/>
+<link rel="manifest" href="assets/images/favicons/site.webmanifest" />
+<meta name="description" content="Frosty Business" />
