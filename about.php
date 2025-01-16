1f2d7b14f66fd329ad439f14ed8c082cd6af9d66
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2025-01-16 13:55:21 +0530

    s73

--- about.php
+++ about.php
@@ -63,7 +63,7 @@
 
         <!--Page Header Start-->
         <section class="page-header">
-            <div class="page-header-bg" style="background-image: url(assets/images/backgrounds/page-header-bg.jpg)">
+            <div class="page-header-bg" style="background-image: url(#)">
             </div>
 
             <div class="container">
